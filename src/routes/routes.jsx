bf69f0882de3cc8f353805d1db92ca787988f116
ummesalma303 <ummesalma333999@gmail.com>
2024-11-11 07:51:14 +0600

    Addeed basic setup with added three pages

---
+++ src/routes/routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'/register',
+            element:<Register></Register>
+        },
+        {
+            path:'/login',
+            element:<Login></Login>
+        }
+      ]
+    },
+  ]);
+export {router}
